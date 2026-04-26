cc3d96ca88ac5331260ab5bb2aaa40134608c1bf
Rutuja Gaikwad <rutujag44@gmail.com>
2026-04-26 16:52:57 +0530

    navbar for m-site fixed

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react';
-import { Container, Navbar, Nav,Stack } from 'react-bootstrap';
+import { Container, Navbar, Nav,Stack, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import { Notification } from './Chat/Notification';
@@ -15,19 +15,46 @@ export const NavBar = () => {
             Chat-App
             </Link>
             </h2>
-            {user && (
+            {/* {user && (
                 <span className='text-warning'>Logged in as {user.name}</span>
-            )}
+            )} */}
             <Nav>
                 <Stack direction='horizontal' gap={3}>
-                {user ? (
+                {/* {user ? (
                     <>
                         <Notification />
                         <Link className='text-decoration-none link-light' onClick={() => logoutUser()} to="/login">
                             Logout
                         </Link>
                     </>
-                ) : (
+                ) : ( */}
+                {user ? (
+  <>
+    <Notification />
+
+    <Dropdown align="end">
+      <Dropdown.Toggle
+        variant="dark"
+        id="dropdown-basic"
+        className="three-dot-toggle"
+      >
+        ⋮
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item className='username' disabled>
+          {user.name}
+        </Dropdown.Item>
+
+        <Dropdown.Divider />
+
+        <Dropdown.Item className='logout' onClick={logoutUser}>
+          Logout
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  </>
+) : (
                     <>
                         <Link to="/login" className='text-decoration-none link-light'>
                             Login
